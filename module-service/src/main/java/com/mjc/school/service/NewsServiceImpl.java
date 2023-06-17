b76a28e66b8c08a4bc7eee691e998669501074bf
SirJohanot <pavelkrafft@gmail.com>
2023-06-17 22:24:46 +0300

    added some implementation to the NewsServiceImpl

--- module-service/src/main/java/com/mjc/school/service/NewsServiceImpl.java
+++ module-service/src/main/java/com/mjc/school/service/NewsServiceImpl.java
@@ -1,7 +1,10 @@
 package com.mjc.school.service;
 
+import com.mjc.school.repository.AuthorModel;
+import com.mjc.school.repository.NewsModel;
 import com.mjc.school.repository.NewsRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class NewsServiceImpl implements NewsService {
@@ -14,26 +17,54 @@ public class NewsServiceImpl implements NewsService {
 
     @Override
     public NewsDto createNews(NewsDto newsDto) {
-        return null;
+        newsDto.setCreateDate(LocalDateTime.now());
+        newsDto.setLastUpdateDate(LocalDateTime.now());
+        return newsModelToNewsDto(repository.createNews(newsDtoToNewsModel(newsDto)));
     }
 
     @Override
     public List<NewsDto> getAllNews() {
-        return null;
+        return repository.getAllNews()
+                .stream()
+                .map(this::newsModelToNewsDto)
+                .toList();
     }
 
     @Override
     public NewsDto getNewsById(Long newsId) {
-        return null;
+        return newsModelToNewsDto(repository.getNewsById(newsId));
     }
 
     @Override
     public NewsDto updateNews(NewsDto newsDto) {
-        return null;
+        newsDto.setLastUpdateDate(LocalDateTime.now());
+        return newsModelToNewsDto(repository.updateNews(newsDtoToNewsModel(newsDto)));
     }
 
     @Override
     public boolean deleteNews(Long newsId) {
-        return false;
+        return repository.deleteNews(newsId);
+    }
+
+    private NewsModel newsDtoToNewsModel(NewsDto newsDto) {
+        Long id = newsDto.getId();
+        String title = newsDto.getTitle();
+        String content = newsDto.getContent();
+        LocalDateTime createDate = newsDto.getCreateDate();
+        LocalDateTime lastUpdateDate = newsDto.getLastUpdateDate();
+        Long authorId = newsDto.getAuthorId();
+        String authorName = newsDto.getAuthorName();
+        return new NewsModel(id, title, content, createDate, lastUpdateDate, new AuthorModel(authorId, authorName));
+    }
+
+    private NewsDto newsModelToNewsDto(NewsModel newsModel) {
+        Long id = newsModel.getId();
+        String title = newsModel.getTitle();
+        String content = newsModel.getContent();
+        LocalDateTime createDate = newsModel.getCreateDate();
+        LocalDateTime lastUpdateDate = newsModel.getLastUpdateDate();
+        Long authorId = newsModel.getAuthorModel().getId();
+        String authorName = newsModel.getAuthorModel().getName();
+        return new NewsDto(id, title, content, createDate, lastUpdateDate, authorId, authorName);
     }
 }
