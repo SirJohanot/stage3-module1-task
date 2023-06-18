b95f2571385a32bbb0e795196cb22c29f82ffb4d
SirJohanot <pavelkrafft@gmail.com>
2023-06-18 21:42:38 +0300

    renamed NewsModel.authorModel to NewsModel.author

--- module-repository/src/main/java/com/mjc/school/repository/NewsModel.java
+++ module-repository/src/main/java/com/mjc/school/repository/NewsModel.java
@@ -10,15 +10,15 @@ public class NewsModel {
     private final String content;
     private final LocalDateTime createDate;
     private final LocalDateTime lastUpdateDate;
-    private final AuthorModel authorModel;
+    private final AuthorModel author;
 
-    public NewsModel(Long id, String title, String content, LocalDateTime createDate, LocalDateTime lastUpdateDate, AuthorModel authorModel) {
+    public NewsModel(Long id, String title, String content, LocalDateTime createDate, LocalDateTime lastUpdateDate, AuthorModel author) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.createDate = createDate;
         this.lastUpdateDate = lastUpdateDate;
-        this.authorModel = authorModel;
+        this.author = author;
     }
 
     public Long getId() {
@@ -41,8 +41,8 @@ public class NewsModel {
         return lastUpdateDate;
     }
 
-    public AuthorModel getAuthorModel() {
-        return authorModel;
+    public AuthorModel getAuthor() {
+        return author;
     }
 
     @Override
@@ -54,12 +54,12 @@ public class NewsModel {
             return false;
         }
         NewsModel newsModel = (NewsModel) o;
-        return Objects.equals(id, newsModel.id) && Objects.equals(title, newsModel.title) && Objects.equals(content, newsModel.content) && Objects.equals(createDate, newsModel.createDate) && Objects.equals(lastUpdateDate, newsModel.lastUpdateDate) && Objects.equals(authorModel, newsModel.authorModel);
+        return Objects.equals(id, newsModel.id) && Objects.equals(title, newsModel.title) && Objects.equals(content, newsModel.content) && Objects.equals(createDate, newsModel.createDate) && Objects.equals(lastUpdateDate, newsModel.lastUpdateDate) && Objects.equals(author, newsModel.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, content, createDate, lastUpdateDate, authorModel);
+        return Objects.hash(id, title, content, createDate, lastUpdateDate, author);
     }
 
     @Override
@@ -70,7 +70,7 @@ public class NewsModel {
                 ", content='" + content + '\'' +
                 ", createDate=" + createDate +
                 ", lastUpdateDate=" + lastUpdateDate +
-                ", authorModel=" + authorModel +
+                ", author=" + author +
                 '}';
     }
 }
