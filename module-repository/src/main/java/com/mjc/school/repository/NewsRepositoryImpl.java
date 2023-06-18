dbcd61e8ee7d72badadd45d37dbdca192305be39
SirJohanot <pavelkrafft@gmail.com>
2023-06-18 21:42:59 +0300

    created initial implementation for NewsRepositoryImpl

---
+++ module-repository/src/main/java/com/mjc/school/repository/NewsRepositoryImpl.java
@@ -0,0 +1,100 @@
+package com.mjc.school.repository;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class NewsRepositoryImpl implements NewsRepository {
+
+    private Long newsIdCounter;
+    private final List<NewsModel> newsModelList;
+    private final List<AuthorModel> authorModelList;
+
+    public NewsRepositoryImpl() {
+        authorModelList = new LinkedList<>();
+        Scanner authorsScanner = new Scanner(this.getClass().getResourceAsStream("author.txt"));
+        for (long i = 1; authorsScanner.hasNext(); i++) {
+            String authorName = authorsScanner.nextLine();
+            authorModelList.add(new AuthorModel(i, authorName));
+        }
+
+        List<String> contentList = new ArrayList<>();
+        Scanner contentScanner = new Scanner(this.getClass().getResourceAsStream("content.txt"));
+        while (contentScanner.hasNext()) {
+            contentList.add(contentScanner.nextLine());
+        }
+        newsModelList = new LinkedList<>();
+        Scanner newsScanner = new Scanner(this.getClass().getResourceAsStream("news.txt"));
+        long i = 1;
+        for (; newsScanner.hasNext(); i++) {
+            String newsTitle = newsScanner.nextLine();
+
+            String newsContentNumberLine = newsScanner.nextLine();
+            int newsContentNumber = Integer.parseInt(newsContentNumberLine);
+            String newsContent = contentList.get(newsContentNumber);
+
+            String newsCreateLine = newsScanner.nextLine();
+            LocalDateTime newsCreateDate = LocalDateTime.parse(newsCreateLine);
+
+            String newsLastUpdateLine = newsScanner.nextLine();
+            LocalDateTime newsLastUpdateDate = LocalDateTime.parse(newsLastUpdateLine);
+
+            String newsAuthorNumberLine = newsScanner.nextLine();
+            int newsAuthorNumber = Integer.parseInt(newsAuthorNumberLine);
+            AuthorModel newsAuthor = authorModelList.get(newsAuthorNumber);
+
+            newsModelList.add(new NewsModel(i, newsTitle, newsContent, newsCreateDate, newsLastUpdateDate, newsAuthor));
+        }
+        newsIdCounter = i;
+    }
+
+    @Override
+    public NewsModel createNews(NewsModel newsModel) {
+        String title = newsModel.getTitle();
+        String content = newsModel.getContent();
+        LocalDateTime createDate = newsModel.getCreateDate();
+        LocalDateTime lastUpdateDate = newsModel.getLastUpdateDate();
+        Long authorId = newsModel.getId();
+        AuthorModel author = authorModelList.stream()
+                .filter(a -> Objects.equals(a.getId(), authorId))
+                .findFirst()
+                .orElse(null);
+        NewsModel newsToBeAdded = new NewsModel(newsIdCounter++, title, content, createDate, lastUpdateDate, author);
+        newsModelList.add(newsToBeAdded);
+        return newsToBeAdded;
+    }
+
+    @Override
+    public List<NewsModel> getAllNews() {
+        return newsModelList;
+    }
+
+    @Override
+    public NewsModel getNewsById(Long newsId) {
+        return newsModelList.stream()
+                .filter(n -> Objects.equals(n.getId(), newsId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public NewsModel updateNews(NewsModel newsModel) {
+        Long id = newsModel.getId();
+        String title = newsModel.getTitle();
+        String content = newsModel.getContent();
+        LocalDateTime createDate = newsModel.getCreateDate();
+        LocalDateTime lastUpdateDate = newsModel.getLastUpdateDate();
+        Long authorId = newsModel.getId();
+        AuthorModel author = authorModelList.stream()
+                .filter(a -> Objects.equals(a.getId(), authorId))
+                .findFirst()
+                .orElse(null);
+        NewsModel newsToBeUpdated = new NewsModel(id, title, content, createDate, lastUpdateDate, author);
+        newsModelList.replaceAll(n -> Objects.equals(n.getId(), id) ? newsToBeUpdated : n);
+        return newsToBeUpdated;
+    }
+
+    @Override
+    public boolean deleteNews(Long newsId) {
+        return newsModelList.removeIf(n -> Objects.equals(n.getId(), newsId));
+    }
+}
